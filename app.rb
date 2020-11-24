12216a7288122702cab86bd9af2a913fa96bf242
erikn775 <erikn775@gmail.com>
2020-11-24 09:11:58 -0700

    commit tree 0657ef44e74feb180fdbeb8a51f0d390a8cb803e

--- app.rb
+++ app.rb
@@ -1,6 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
+  
+  get "/reversename/:name" do
+    @user_name = params[:name].reverse
+    "Hello #{@user_name}!"
+  end
 
+  get "/square/:number" do
+    num1 = params[:number].to_i
+    num2 = num1 ** 2
+    "#{num2}"
+  end
+
+  get "/say/:number/:phrase" do
+    sentence = params[:phrase]
+    num1 = params[:number].to_i
+    num1.times {sentence}
+  end
 end
